adc7032c3f374de91210657fee0dae8688ac8f8d
Tomabage <tamaoona@163.com>
2021-02-20 18:43:32 +0800

    添加createdTime数据 修改单词拼写错误

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    {{ record }}
-    <NumberPad :value.sync="record.amount" @sumbmit="saveRecord"/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -16,11 +15,14 @@ import Notes from '@/components/Money/Notes.vue'
 import Tags from '@/components/Money/Tags.vue'
 import {Component,Watch} from 'vue-property-decorator'
 
+const recordList: Record[]=JSON.parse(window.localStorage.getItem('recordList') ||'[]')
+
 type Record = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
+  createdTime?: Date;
 }
 
 @Component({
@@ -28,7 +30,7 @@ type Record = {
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行']
-  recordList: Record[]=[]
+  recordList: Record[]= recordList
   record: Record = {
     tags: [],
     notes: '',
@@ -45,9 +47,9 @@ export default class Money extends Vue {
   }
 
   saveRecord(){
-    const record2=JSON.parse(JSON.stringify(this.record))
+    const record2: Record=JSON.parse(JSON.stringify(this.record))
+    record2.createdTime=new Date()
     this.recordList.push(record2)
-    console.log(record2)
   }
   @Watch('recordList')
   onRecordListChange(){
