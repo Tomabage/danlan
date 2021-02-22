51a2106f59e5e2471d714dd72fc0ff464b8145ca
Tomabage <tamaoona@163.com>
2021-02-22 16:22:56 +0800

    将Notes组件改造成通用组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,25 +1,32 @@
 <template>
-    <Layout>
-      <div>编辑标签</div>
-    </Layout>
+  <Layout>
+    <div>
+      <Icon name="left"/>
+      <span>编辑标签</span>
+    </div>
+    <Notes field-name="标签名" placeholder="请输入标签名"/>
+  </Layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator'
 import tagListModel from '@/models/tagListModel'
+import Notes from '@/components/Money/Notes.vue'
 
-@Component
-export default class EditLabel extends Vue{
+@Component({
+  components: {Notes}
+})
+export default class EditLabel extends Vue {
 
-  created(){
+  created() {
     const id = this.$route.params.id
     tagListModel.fetch()
-    const tags =tagListModel.data
-    const tag = tags.filter(t=>t.id===id)[0]
-    if(tag){
+    const tags = tagListModel.data
+    const tag = tags.filter(t => t.id === id)[0]
+    if (tag) {
       console.log(tag)
-    } else{
+    } else {
       this.$router.replace('/404')
     }
   }
