4245eaa6494194c763b067781b20e2e27df455aa
Tomabage <tamaoona@163.com>
2021-02-24 09:25:28 +0800

    显示标签的name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,12 +1,12 @@
 <template>
   <Layout>
-   <div class="navBar">
-     <Icon class="leftIcon" name="left"/>
-     <span class="title">编辑标签</span>
-     <span class="rightIcon"></span>
-   </div>
+    <div class="navBar">
+      <Icon class="leftIcon" name="left"/>
+      <span class="title">编辑标签</span>
+      <span class="rightIcon"></span>
+    </div>
     <div class="form-wrapper">
-      <FormItem field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
       <Button>删除标签</Button>
@@ -25,6 +25,7 @@ import Button from '@/components/Button.vue'
   components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
+  tag?: { id: string; name: string } = undefined
 
   created() {
     const id = this.$route.params.id
@@ -32,7 +33,7 @@ export default class EditLabel extends Vue {
     const tags = tagListModel.data
     const tag = tags.filter(t => t.id === id)[0]
     if (tag) {
-      console.log(tag)
+      this.tag = tag
     } else {
       this.$router.replace('/404')
     }
@@ -41,7 +42,7 @@ export default class EditLabel extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.navBar{
+.navBar {
   text-align: center;
   font-size: 16px;
   padding: 12px 16px;
@@ -49,20 +50,24 @@ export default class EditLabel extends Vue {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  >.leftIcon{
+
+  > .leftIcon {
     width: 24px;
     height: 24px;
   }
-  >.rightIcon{
+
+  > .rightIcon {
     width: 24px;
     height: 24px;
   }
 }
-.form-wrapper{
+
+.form-wrapper {
   background: white;
   margin-top: 8px;
 }
-.button-wrapper{
+
+.button-wrapper {
   text-align: center;
   padding: 16px;
   margin-top: 28px;
