15af5471491b64a7cc5e9aa6bb429172bdfbb585
Tomabage <tamaoona@163.com>
2021-03-01 12:01:21 +0800

    重点：使用Vuex数据管理 改写store

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,13 +16,15 @@ import Types from '@/components/Money/Types.vue'
 import FormItem from '@/components/Money/FormItem.vue'
 import Tags from '@/components/Money/Tags.vue'
 import {Component} from 'vue-property-decorator'
-import store from '@/store/index2'
 
 @Component({
   components: {FormItem, Tags, Types, NumberPad}
 })
 export default class Money extends Vue {
-  recordList = store.recordList
+  get recordList() {
+    return this.$store.state.recordList
+  }
+
   record: RecordItem = {
     tags: [],
     notes: '',
@@ -30,12 +32,16 @@ export default class Money extends Vue {
     amount: 0
   }
 
+  created() {
+    this.$store.commit('fetchRecords')
+  }
+
   onUpdateNotes(value: string) {
     this.record.notes = value
   }
 
   saveRecord() {
-    store.createRecord(this.record)
+    this.$store.commit('createRecord', this.record)
   }
 
 }
