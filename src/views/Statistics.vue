04b4de8819270b1efbb394a0ddf79ac0457951ab
Tomabage <tamaoona@163.com>
2021-03-01 17:05:33 +0800

    删除Types组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -9,13 +9,12 @@
 import Tabs from '@/components/Tabs.vue'
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator'
-import Types from '@/components/Money/Types.vue'
 import intervalList from '@/constants/intervalList'
 import recordTypeList from '@/constants/recordTypeList'
 
 
 @Component({
-  components: {Tabs, Types}
+  components: {Tabs}
 })
 export default class Statistics extends Vue {
   type = '-'
