a06aa7ba78c01735fc272b0621148adbb9bdbee2
Tomabage <tamaoona@163.com>
2021-03-03 11:51:48 +0800

    优化PC界面样式

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
 <style lang="scss">
 @import "~@/assets/style/helper.scss";
 @import "~@/assets/style/reset.scss";
-body{
+
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
@@ -15,5 +16,10 @@ body{
   font-size: 16px;
   font-family: $font-hei;
   background: #f5f5f5;
+}
+
+#app {
+  max-width: 500px;
+  margin: 0 auto;
 }
 </style>
